290fdf7a925924ff05207bf1884c071dcbb6af67
Ivan Mendes <ivanmendes2005@gmail.com>
2024-10-22 21:11:00 +0000

    feat: Alterações realizadas para exibir documentação no navegador

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import Control.Monad (void)
 import qualified Web.Scotty as Scotty       -- Necessário pois o método get do scotty entra em conflito com get do persistent
 
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.Static (addBase, staticPolicy)
 
 import Models.Product
 import Routes.Routes(routes)                --confuso
@@ -21,33 +22,5 @@ main = do
     putStrLn "Servidor iniciado em http://localhost:3000"
     Scotty.scotty 3000 $ do
         Scotty.middleware logStdoutDev
+        Scotty.middleware $ staticPolicy (addBase "static")
         routes
-        
-    -- runSqlite "apiHaskell.db" $ do
-        -- Executa as migrações
-        -- void $ runMigration migrateAll
-
-    -- Pega todos os produtos
-        -- products <- getAllProducts
-        -- liftIO $ print (products :: [(Key Products, Products)])
-    -- Pega um produto pelo id
-        -- products <- getProductById 2
-        -- liftIO $ print (products :: Maybe Products)
-    -- Cria um novo produto
-        -- let newProduct = ProductData "Televisão" "Sony" 12000.00
-        -- products <- createProduct newProduct
-        -- liftIO $ print products
-    -- Deleta um produto
-        -- deleted <- deleteProduct 3
-        -- if deleted
-            -- then liftIO $ putStrLn "Produto deletado com sucesso."
-            -- else liftIO $ putStrLn "Nenhum produto foi encontrado para deletar."
-    -- Atualiza o campo name de um produto
-        -- products <- updateProductName 2 "Computador"
-        -- liftIO $ print products
-    -- Atualiza o campo brand de um produto
-        -- products <- updateProductBrand 2 "Dell"
-        -- liftIO $ print products
-    -- Atualiza o campo price de um produto
-        -- products <- updateProductPrice 2 3000.00
-        -- liftIO $ print products
--- src/Routes/Routes.hs
+++ src/Routes/Routes.hs
@@ -3,11 +3,13 @@
 module Routes.Routes where
 
 import Web.Scotty
+import Data.Text.Lazy (pack)
 
 import Routes.ProductsRoutes(productsRoutes)
 
 routes :: ScottyM ()
 routes = do
-    get "/" $ html "Hello, world!"
-    get "/about" $ html "About page"
+    get "/" $ do
+        htmlFile <- liftIO $ readFile "./src/Public/Html/index.html"
+        html $ mconcat [pack htmlFile]
     productsRoutes
